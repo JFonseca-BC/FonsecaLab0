3514df6bb6b17c7f42be31e77cc851e35bcd3cad
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-13 00:41:01 -0800

    Copied code example to build off

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/* Note: this code snippet is gvingn you some tips to start
+you cannot simply copy this code.
+Also your code has to be more modular and create more functions
+*/
+let arrayButtons = [];
+function Button(color, width, height, top, left, order) {
+    this.order = order;
+    this.btn = document.createElement("button");
+    this.btn.style.backgroundColor = color;
+    this.btn.style.width = width;
+    this.btn.style.height = height;
+    this.btn.style.position = "absolute";
+
+    document.body.appendChild(this.btn);
+    // A method to set location
+    this.setLocation = function (top, left) {
+        this.btn.style.top = top;
+        this.btn.style.left = left;
+    };
+    /* we call this method to set original
+    top, left during creation of object */
+    this.setLocation(top, left);
+
+}
+
+arrayButtons.push(new Button("Red", "100px", "100px", "0px", "0px", 0));
+arrayButtons.push(new Button("Blue", "200px", "100px", "200px", "200px", 0));
+// this has to be in a separate function, e.g. moveRandom ... or something
+setInterval(function () {
+    arrayButtons[0].setLocation(
+        Math.floor(Math.random() * 100) + "px",
+        Math.floor(Math.random() * 100) + "px");
+}, 500);
