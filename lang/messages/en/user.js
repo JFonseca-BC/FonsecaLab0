5ec2dd7286891db7fd3371064c47499ad79a58c6
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-13 08:17:45 -0800

    Moved title to user.js

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -2,10 +2,12 @@ const gamePrompt = "How many buttons to create?";
 const gameWin = "Excellent memory!";
 const gameLose = "Wrong order!";
 const goButton = "Go!";
+const title = "Lab 0"
 
 const strings = {
   gamePrompt,
   gameWin,
   gameLose,
   goButton,
+  title,
 };
