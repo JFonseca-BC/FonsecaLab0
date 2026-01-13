65a3835f18386d75b3bcb2e2429e8debabd94248
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-13 08:37:36 -0800

    Moved title back to index.html because I forgot how server-side js works

--- lang/messages/en/user.js
+++ lang/messages/en/user.js
@@ -2,12 +2,10 @@ const gamePrompt = "How many buttons to create?";
 const gameWin = "Excellent memory!";
 const gameLose = "Wrong order!";
 const goButton = "Go!";
-const title = "Lab 0"
 
 const strings = {
   gamePrompt,
   gameWin,
   gameLose,
   goButton,
-  title,
 };
