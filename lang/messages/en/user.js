754221d4bb6c14b651b1d24aefe385bc241f2d06
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-13 00:31:31 -0800

    Started basics

---
+++ lang/messages/en/user.js
@@ -0,0 +1,11 @@
+const gamePrompt = "How many buttons to create?";
+const gameWin = "Excellent memory!";
+const gameLose = "Wrong order!";
+const goButton = "Go!";
+
+const strings = {
+  gamePrompt,
+  gameWin,
+  gameLose,
+  goButton,
+};
