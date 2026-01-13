195381bd8d95eb73604810bd5a8a4daf586fd261
JFonseca-BC <jfonseca10@my.bcit.ca>
2026-01-13 08:42:50 -0800

    Wrote a simple node express app

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 3000 is a test port!
+const PORT = process.env.PORT || 3000;
+
+// Static paths for serving files
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/lang', express.static(path.join(__dirname, 'lang')));
+
+// Serves the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Starts the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
